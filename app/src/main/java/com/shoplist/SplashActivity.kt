a45c609d80cfaf7282a71e2d0bb8f84e1e840c87
Sphelele <sphemicah@gmail.com>
2020-08-23 16:19:56 +0200

    - ✔ Added splash screen activity

--- app/src/main/java/com/shoplist/SplashActivity.kt
+++ app/src/main/java/com/shoplist/SplashActivity.kt
@@ -1,9 +1,11 @@
-package com
+package com.shoplist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shoplist.R
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
