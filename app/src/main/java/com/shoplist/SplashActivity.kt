19f5addfe1f74a6ccdaa73453420f3101bc03832
Sphelele <sphemicah@gmail.com>
2020-08-25 00:05:36 +0200

    - ✨ Added subtitles on Splash screen

--- app/src/main/java/com/shoplist/SplashActivity.kt
+++ app/src/main/java/com/shoplist/SplashActivity.kt
@@ -25,7 +25,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         Completable.timer(
-            2, TimeUnit.SECONDS,
+            3, TimeUnit.SECONDS,
             AndroidSchedulers.mainThread()
         ).subscribe {
             startActivity(Intent(this,MainActivity::class.java))
