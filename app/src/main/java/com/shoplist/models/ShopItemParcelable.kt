ebfed2e7fada248b4c279500f93cdb80d0e983b8
Sphelele <sphemicah@gmail.com>
2020-09-15 23:17:54 +0200

    - ✨ Fixed some code formatting

--- app/src/main/java/com/shoplist/models/ShopItemParcelable.kt
+++ app/src/main/java/com/shoplist/models/ShopItemParcelable.kt
@@ -4,8 +4,9 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class ShopItemParcelable(var id:Int,var name:String,
-                              var dateAdded:String,var quantity:Int,
-                              var categoryId:Int,var itemCost:Double,
-                              var isMarked:Boolean) : Parcelable {
-}
+data class ShopItemParcelable(
+    var id: Int, var name: String,
+    var dateAdded: String, var quantity: Int,
+    var categoryId: Int, var itemCost: Double,
+    var isMarked: Boolean
+) : Parcelable 
