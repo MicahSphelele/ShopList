420c7dbc44c0b3377fd9fe90b22b02dd0740692c
Sphelele <sphemicah@gmail.com>
2021-08-17 08:10:57 +0200

    - Updated RepoModule  📦

--- app/src/main/java/com/shoplist/di/RepoModule.kt
+++ app/src/main/java/com/shoplist/di/RepoModule.kt
@@ -7,16 +7,19 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object RepoModule {
 
+    @Singleton
     @Provides
     fun providesCategoryRepo(application: Application): CategoryRepo {
         return CategoryRepo(application)
     }
 
+    @Singleton
     @Provides
     fun providesShopItemRepo(application: Application): ShopItemRepo {
         return ShopItemRepo(application)
