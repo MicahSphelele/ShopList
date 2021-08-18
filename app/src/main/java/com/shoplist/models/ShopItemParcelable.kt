eefb755845e122fe645e135f7a76499cd47bb926
Sphelele <sphemicah@gmail.com>
2021-08-18 13:10:47 +0200

    - Edited ShopItemParcelable  💻

--- app/src/main/java/com/shoplist/models/ShopItemParcelable.kt
+++ app/src/main/java/com/shoplist/models/ShopItemParcelable.kt
@@ -5,8 +5,11 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ShopItemParcelable(
-    var id: Int, var name: String,
-    var dateAdded: String, var quantity: Int,
-    var categoryId: Int, var itemCost: Double,
+    var id: Int,
+    var name: String,
+    var dateAdded: String,
+    var quantity: Int,
+    var categoryId: Int,
+    var itemCost: Double,
     var isMarked: Boolean
 ) : Parcelable
