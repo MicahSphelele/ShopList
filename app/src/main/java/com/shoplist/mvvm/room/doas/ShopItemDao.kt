fc83430ae2a0277c4b5e8f43fa719e761e48645c
Sphelele <sphemicah@gmail.com>
2021-08-18 11:42:21 +0200

    - Code clean up on ShopItemDao

--- app/src/main/java/com/shoplist/mvvm/room/doas/ShopItemDao.kt
+++ app/src/main/java/com/shoplist/mvvm/room/doas/ShopItemDao.kt
@@ -9,20 +9,20 @@ import com.shoplist.util.Constants
 interface ShopItemDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(shopItem: ShopItem) : Long
+    suspend fun insert(shopItem: ShopItem): Long
 
     @Update
-    suspend fun update(shopItem: ShopItem) : Int
+    suspend fun update(shopItem: ShopItem): Int
 
     @Delete
-    suspend fun delete(shopItem: ShopItem) : Int
+    suspend fun delete(shopItem: ShopItem): Int
 
     @Query("SELECT * FROM ${Constants.SHOP_TABLE}")
-    fun getAllShoppingItems() : LiveData<List<ShopItem>>
+    fun getAllShoppingItems(): LiveData<List<ShopItem>>
 
     @Query("SELECT SUM(${Constants.SHOP_ITEM_COST}*${Constants.SHOP_QUANTITY}) FROM ${Constants.SHOP_TABLE}")
-    fun getTotalEstimationCost() : LiveData<Double>
+    fun getTotalEstimationCost(): LiveData<Double>
 
     @Query("SELECT SUM(${Constants.SHOP_IS_MARKED}) FROM ${Constants.SHOP_TABLE}")
-    fun getTotalMarkedItems() : LiveData<Int>
+    fun getTotalMarkedItems(): LiveData<Int>
 }
