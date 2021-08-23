c31119ab3755969bc96ca61cf5774bf2b07cacdb
Sphelele <sphemicah@gmail.com>
2021-08-23 08:09:00 +0200

    - Created Extension function  💻

--- app/src/main/java/com/shoplist/util/Extensions.kt
+++ app/src/main/java/com/shoplist/util/Extensions.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
@@ -21,6 +22,11 @@ fun ViewGroup.viewHolderItemBinding(resId: Int): ViewDataBinding {
 
 fun Context.getViewBinder(layout: Int): ViewDataBinding {
     return DataBindingUtil.inflate(LayoutInflater.from(this), layout, null, false)
+}
+
+fun Context.hideDeviceSoftKeyboard(view: View) {
+    val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
 }
 
 
