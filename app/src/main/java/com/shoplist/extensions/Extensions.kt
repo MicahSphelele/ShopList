0f87f61dd6924aded9d2a4f1fbdee9378601da89
Sphelele <sphemicah@gmail.com>
2021-09-10 15:22:00 +0200

    - added showPopupMenu extended function  💻

--- app/src/main/java/com/shoplist/extensions/Extensions.kt
+++ app/src/main/java/com/shoplist/extensions/Extensions.kt
@@ -2,15 +2,23 @@ package com.shoplist.extensions
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.annotation.LayoutRes
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import com.google.android.material.imageview.ShapeableImageView
+import com.shoplist.R
+import com.shoplist.databinding.ItemShopBinding
+import com.shoplist.presentation.ui.adapters.ShopItemAdapter
+import com.shoplist.presentation.ui.adapters.ShopItemAdapter.*
 
 fun ViewGroup.viewHolderItemBinding(@LayoutRes resId: Int): ViewDataBinding {
     return DataBindingUtil.inflate(
@@ -19,6 +27,28 @@ fun ViewGroup.viewHolderItemBinding(@LayoutRes resId: Int): ViewDataBinding {
         this,
         false
     )
+}
+
+fun ItemShopBinding.showPopupMenu(view: View,itemSelected: (ShopItemAction) -> Unit ) {
+    val popup =
+        PopupMenu(ContextThemeWrapper(this.root.context, R.style.ItemPopUpMenuStyle), view)
+
+    popup.menuInflater.inflate(R.menu.shop_item_menu, popup.menu)
+
+    if (popup.menu is MenuBuilder) {
+        (popup.menu as MenuBuilder).setOptionalIconsVisible(true)
+    }
+
+    popup.setOnMenuItemClickListener { item: MenuItem ->
+        if (item.itemId == R.id.item_edit) {
+            itemSelected(ShopItemAction.EDIT)
+        }
+        if (item.itemId == R.id.item_delete) {
+            itemSelected(ShopItemAction.DELETE)
+        }
+        true
+    }
+    popup.show()
 }
 
 fun Context.getViewBinder(layout: Int): ViewDataBinding {
