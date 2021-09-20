c4ff7c5eda3b88717bf45d6da70094b41fd9a5c5
Sphelele <sphemicah@gmail.com>
2021-09-20 09:25:51 +0200

    - Created AddOrEditViewModel  💻

---
+++ app/src/main/java/com/shoplist/presentation/ui/fragments/addoredit/AddOrEditViewModel.kt
@@ -0,0 +1,53 @@
+package com.shoplist.presentation.ui.fragments.addoredit
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.shoplist.domain.models.Category
+import com.shoplist.domain.models.ShopItem
+import com.shoplist.domain.usecases.category.GetAllCategoriesUseCase
+import com.shoplist.domain.usecases.category.GetCategoryByIdUseCase
+import com.shoplist.domain.usecases.shopitems.InsertShopItemUseCase
+import com.shoplist.domain.usecases.shopitems.UpdateShopItemUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AddOrEditViewModel @Inject constructor(
+    private val getAllCategoriesUseCase: GetAllCategoriesUseCase,
+    private val getCategoryByIdUseCase: GetCategoryByIdUseCase,
+    private val insertShopItemUseCase: InsertShopItemUseCase,
+    private val updateShopItemUseCase: UpdateShopItemUseCase
+) : ViewModel() {
+
+    fun getAllCategories(
+        lifecycleOwner: LifecycleOwner,
+        onAllCategoryItems: (List<Category>?) -> Unit
+    ) {
+        getAllCategoriesUseCase().removeObservers(lifecycleOwner)
+        getAllCategoriesUseCase().observe(lifecycleOwner) {
+            onAllCategoryItems(it)
+        }
+    }
+
+    fun getCategoryById(
+        lifecycleOwner: LifecycleOwner,
+        id: Int,
+        onCategoryById: (Category?) -> Unit
+    ) {
+        getCategoryByIdUseCase(viewModelScope, id) {
+            onCategoryById(it)
+        }
+
+    }
+
+    fun insertShopItem(shopItem: ShopItem) {
+        insertShopItemUseCase(viewModelScope, shopItem)
+    }
+
+    fun updatedShopItem(shopItem: ShopItem, onShopItemUpdated: () -> Unit) {
+        updateShopItemUseCase(viewModelScope, shopItem) {
+            onShopItemUpdated()
+        }
+    }
+}
